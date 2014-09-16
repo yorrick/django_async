ac59554640aad7e70990700ed8ccb3a9e9ac106b
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-09-16 11:51:37 -0400

    removed ansible conf

--- django_async/settings_prod.py
+++ django_async/settings_prod.py
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'localhost', 
+        'HOST': 'localhost',
         'PORT': 5432,
         }
 }
